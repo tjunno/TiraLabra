275ee0b8b0c2d2a14a24fcc10fb32e8c0184260c
Tuomas Junno <tuomas.junno@cs.helsinki.fi>
2017-11-05 15:08:48 +0200

    testing

---
+++ labyrinth/src/main/java/labyrinth/Labyrinth.java
@@ -0,0 +1,27 @@
+package labyrinth;
+
+/**
+ *
+ * @author Tuomas Junno
+ */
+public class Labyrinth {
+    
+    /**
+     * Testing Gradle, Travis and Jacoco
+     */
+    
+    public Labyrinth(){
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Labyrinth labyrinth = new Labyrinth();       
+    }
+
+
+    
+}
